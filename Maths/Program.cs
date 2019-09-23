1f56ac9da4397bd6cba1916731cc5ed0c2323662
Andrea Tseka <5206114@COMPUTING-GLOS.local>
2019-09-23 12:33:42 +0100

    Add project files.

---
+++ Maths/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNum1 = 0;
+            int iNum2 = 0;
+            int iAnswer = 0;
+            Console.WriteLine("Please input the first number that you want to add up ");
+            iNum1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please input the second number that you want to add up");
+            iNum2 = int.Parse(Console.ReadLine());
+
+            iAnswer = iNum1 + iNum2;
+
+            Console.WriteLine("The answer to " + iNum1 + " + " + iNum2 + " is " + iAnswer);
+            //Or Console.Writeline("The answer to {0} + {1} is {2}" , iNum1, iNum2, iAnswer;
+            Console.ReadLine();
+        }
+    }
+}
